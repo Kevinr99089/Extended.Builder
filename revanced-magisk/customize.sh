a2b6688be725a12a4609b382f4b57eaa39c219ab
Kévin <kevin.rdgs@outlook.fr>
2024-09-04 01:33:48 +0200

    Spring cleaning (in summer)
    Synchronising my fork with the j-hc repo

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -128,5 +128,5 @@ ui_print "* Cleanup"
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by Kevinr99089 (github.com/Kevinr99089)"
 ui_print " "
