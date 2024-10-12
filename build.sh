71658e6fcd0f0045acd1337ad0f306e33490c3f9
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2024-10-12 22:15:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ DEF_CLI_VER=$(toml_get "$main_config_t" cli-version) || DEF_CLI_VER="latest"
 DEF_PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || DEF_PATCHES_SRC="inotia00/revanced-patches"
 DEF_INTEGRATIONS_SRC=$(toml_get "$main_config_t" integrations-source) || DEF_INTEGRATIONS_SRC="inotia00/revanced-integrations"
 DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="inotia00/revanced-cli"
-DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="ReVanced Extended"
+DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="Extended"
 mkdir -p $TEMP_DIR $BUILD_DIR
 
 if [ "${2-}" = "--config-update" ]; then
